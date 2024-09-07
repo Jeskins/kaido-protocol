2b08de6e87e741943ff6b2a53cd0648f856b524a
Jeskins <jsjoshua1111@gmail.com>
2024-09-07 14:57:57 +0530

    kinto button fix

--- frontend/src/components/ui/custom/kinto-button.tsx
+++ frontend/src/components/ui/custom/kinto-button.tsx
@@ -3,8 +3,8 @@ import { Button } from "../button";
 
 export default function KintoButton() {
   return (
-    <Button variant={"ghost"} className="space-x-1 font-semibold mx-2">
-      <Image src={"/coins/kinto.png"} width={25} height={25} alt="Kinto" />
+    <Button variant={"ghost"} className="space-x-2 font-semibold mx-2">
+      <Image src={"/coins/kinto.png"} width={20} height={20} alt="Kinto" />
       <p>Kinto Mainnet</p>
     </Button>
   );
